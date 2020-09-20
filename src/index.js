da8f0d6c9e4d96fa78ce6ec2bf4cb298a2851d8e
Tanya <REINACREATES1@GMAIL.com>
2020-09-19 21:32:56 -0400

    updated checkboxes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 let todos = [
     {
         completed: false,
@@ -19,14 +18,37 @@ let todos = [
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
+    let app = document.getElementById("app")
     let h1 = document.createElement("h1")
     // your code here
 
-    h1.innerText = "Todo List"
+    h1.innerText = "Todo List";
 
-    app.appendChild(h1)
+    const TodoListElement = document.createElement('ul');
+
+    app.appendChild(h1);
     // and maybe some here
-}
+    const container = document.createElement("div");
 
+    //header2.innerText = "Take Jim to the hair salon";
+    TodoListElement.classList.add('todo-list');
+
+    for (let i = 0; i< todos.length; i++){
+        const liitem = document.createElement("li")
+        const checkbox = document.createElement("input") 
+        const cblabel = document.createElement("label")
+        checkbox.type = "checkbox"
+        checkbox.name = "todo" + i
+        cblabel.for = "todo" + i
+        cblabel.innerText=todos[i]["description"]
+
+        liitem.appendChild(checkbox)
+        liitem.appendChild(cblabel)
+        TodoListElement.appendChild(liitem)
+     console.log(todos[i]["description"])    
+    }
+    container.appendChild(h1)
+    container.appendChild(TodoListElement)
+    app.appendChild(container)
+}
 renderTodoApp()
