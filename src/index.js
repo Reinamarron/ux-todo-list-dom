bdc59d2fe59df88764f7ad0c0f46a4bcfb24c6f8
Tanya <REINACREATES1@GMAIL.com>
2020-09-19 20:17:43 -0400

    fixed check boxes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ let todos = [
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
+    let app = document.getElementById("app")
     let h1 = document.createElement("h1")
     // your code here
 
@@ -26,21 +26,29 @@ function renderTodoApp() {
 
     const TodoListElement = document.createElement('ul');
 
-    
-
     app.appendChild(h1);
     // and maybe some here
     const container = document.createElement("div");
-    document.body.append(TodoListElement);
-    
 
-    header2.innerText = "Take Jim to the hair salon";
+    //header2.innerText = "Take Jim to the hair salon";
     TodoListElement.classList.add('todo-list');
 
+    for (let i = 0; i< todos.length; i++){
+        const liitem = document.createElement("li")
+        const checkbox = document.createElement("input") 
+        const cblabel = document.createElement("label")
+        checkbox.type = "checkbox"
+        checkbox.name = "todo" + i
+        cblabel.for = "todo" + i
+        cblabel.innerText=todos[i]["description"]
 
-    app.body.appendChild(header);
-    for (let i = 0; i< directory.length; i++){
-        const todo = TodoListElement(1)   
+        liitem.appendChild(checkbox)
+        liitem.appendChild(cblabel)
+        TodoListElement.appendChild(liitem)
+     console.log(todos[i]["description"])    
     }
-
+    container.appendChild(h1)
+    container.appendChild(TodoListElement)
+    app.appendChild(container)
+}
 renderTodoApp()
